73a6202b3b2abb5abffe400b9a77e2e6a985e999
Carlos Duarte <cduarte@lulobank.com>
2024-03-14 15:48:38 -0500

    Move aws_profile variable to tfvars

---
+++ infrastructure/terraform.tfvars
@@ -0,0 +1,1 @@
+aws_profile = "digital-bank-developers"
--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -1,7 +1,7 @@
 variable "aws_region" {
   description = "AWS region"
   type        = string
-  default     = null
+  default     = "us-east-1"
 }
 
 variable "aws_profile" {
